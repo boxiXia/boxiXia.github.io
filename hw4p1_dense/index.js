3feab36965590c959b639aca903719a368e84dac
bx2150@columbia.edu <boxiXia>
2019-04-13 21:53:51 -0400

    colab -> github

---
+++ hw4p1_dense/index.js
@@ -0,0 +1,184 @@
+
+
+
+
+const HOSTED_URLS = {
+  model:
+      'model_js/model.json',
+  metadata:
+      'model_js/metadata.json'
+};
+
+const examples = {
+  'example1':
+      'Once more, the Dover mail struggled on, with the jack-boots of its passengers squashing along by its side.',
+  'example2':
+      'I did not sleep well, though my bed was comfortable enough, for I had all sorts of queer dreams.',
+  'example3':
+      'The peasant woman, perceiving that my mother fixed eyes of wonder and admiration on this lovely girl, eagerly communicated her history.',
+  'example4':
+      'but as I have actually paid the visit, we cannot escape the acquaintance now.',
+  'example5':
+      'Never trust to general impressions, my boy, but concentrate yourself upon details.',
+  'example6':
+      'With this she darted furiously everywhere among the hosts of the Achaeans, urging them forward, and putting courage into the heart of each so that he might fight and do battle without ceasing.',
+};
+
+function status(statusText) {
+  console.log(statusText);
+  document.getElementById('status').textContent = statusText;
+}
+
+function showMetadata(metadataJSON) {
+  document.getElementById('vocabularySize').textContent =
+      metadataJSON['vocabulary_size'];
+  document.getElementById('maxLen').textContent =
+      metadataJSON['max_len'];
+}
+
+function settextField(text, predict) {
+  const textField = document.getElementById('text-entry');
+  textField.value = text;
+  doPredict(predict);
+}
+
+function setPredictFunction(predict) {
+  const textField = document.getElementById('text-entry');
+  textField.addEventListener('input', () => doPredict(predict));
+}
+
+function disableLoadModelButtons() {
+  document.getElementById('load-model').style.display = 'none';
+}
+
+function doPredict(predict) {
+  const textField = document.getElementById('text-entry');
+  const result = predict(textField.value);
+  score_string = "Class scores: ";
+  for (var x in result.score) {
+    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
+  }
+  const prediction_string = 'prediction:'+result.prediction +'
+(';
+  //console.log(score_string);
+  status(
+      prediction_string+score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
+}
+
+function prepUI(predict) {
+  setPredictFunction(predict);
+  const testExampleSelect = document.getElementById('example-select');
+  testExampleSelect.addEventListener('change', () => {
+    settextField(examples[testExampleSelect.value], predict);
+  });
+  settextField(examples['example1'], predict);
+}
+
+async function urlExists(url) {
+  status('Testing url ' + url);
+  try {
+    const response = await fetch(url, {method: 'HEAD'});
+    return response.ok;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function loadHostedPretrainedModel(url) {
+  status('Loading pretrained model from ' + url);
+  try {
+    const model = await tf.loadLayersModel(url);
+    status('Done loading pretrained model.');
+    disableLoadModelButtons();
+    return model;
+  } catch (err) {
+    console.error(err);
+    status('Loading pretrained model failed.');
+  }
+}
+
+async function loadHostedMetadata(url) {
+  status('Loading metadata from ' + url);
+  try {
+    const metadataJson = await fetch(url);
+    const metadata = await metadataJson.json();
+    status('Done loading metadata.');
+    return metadata;
+  } catch (err) {
+    console.error(err);
+    status('Loading metadata failed.');
+  }
+}
+
+class Classifier {
+
+  async init(urls) {
+    this.urls = urls;
+    this.model = await loadHostedPretrainedModel(urls.model);
+    await this.loadMetadata();
+    return this;
+  }
+
+  async loadMetadata() {
+    const metadata =
+        await loadHostedMetadata(this.urls.metadata);
+    showMetadata(metadata);
+    this.maxLen = metadata['max_len'];
+    console.log('maxLen = ' + this.maxLen);
+    this.wordIndex = metadata['word_index'];
+    this.label_names = metadata['label_names'];
+  }
+
+  predict(text) {
+    // Convert to lower case and remove all punctuations.
+    const inputText =
+        text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
+    // Look up word indices.
+    const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
+    for (let i = 0; i < inputText.length; ++i) {
+      const word = inputText[i];
+      inputBuffer.set(this.wordIndex[word], 0, i);
+      //console.log(word, this.wordIndex[word], inputBuffer);
+    }
+    const input = inputBuffer.toTensor();
+    //console.log(input);
+
+    status('Running inference');
+    const beginMs = performance.now();
+    const predictOut = this.model.predict(input);
+    //console.log(predictOut.dataSync());
+    const score = predictOut.dataSync();//[0];
+    predictOut.dispose();
+    const endMs = performance.now();
+
+    // const score_tensor = tf.tensor1d(score).argMax();
+    // score_tensor.argMax().print();
+
+    const prediction_id = tf.tensor1d(score).argMax().dataSync()[0]
+    // let values = tf.tensor1d(score).argMax().map(t => t.dataSync()[0]);
+    // console.log(prediction_id);
+    const prediction = this.label_names[prediction_id];
+    // console.log(prediction);
+
+    return {prediction:prediction,score:score, elapsed: (endMs - beginMs)};
+  }
+};
+
+async function setup() {
+  if (await urlExists(HOSTED_URLS.model)) {
+    status('Model available: ' + HOSTED_URLS.model);
+    const button = document.getElementById('load-model');
+    button.addEventListener('click', async () => {
+      const predictor = await new Classifier().init(HOSTED_URLS);
+      prepUI(x => predictor.predict(x));
+    });
+    button.style.display = 'inline-block';
+  }
+
+  status('Standing by.');
+}
+
+setup();
+
+
+
